31f577c6c075e48b4b0eff240eab722c19ffa711
Endri Kurushi <endri-kurushi@hotmail.com>
2019-02-05 17:24:38 +0100

    Added DTOs and mappers

--- src/main/java/thingy/temp/service/impl/PetServiceImpl.java
+++ src/main/java/thingy/temp/service/impl/PetServiceImpl.java
@@ -1,17 +1,22 @@
 package thingy.temp.service.impl;
 
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import thingy.temp.model.Pet;
 import thingy.temp.repository.PetRepository;
 import thingy.temp.service.PetService;
+import thingy.temp.service.dto.PetDTO;
+import thingy.temp.service.mapper.PetMapper;
 
 @Service
 public class PetServiceImpl implements PetService {
 
     @Autowired
     PetRepository petRepository;
+
+    private PetMapper petMapper = Mappers.getMapper(PetMapper.class);
 
     @Override
     public Pet save(Pet pet) {
