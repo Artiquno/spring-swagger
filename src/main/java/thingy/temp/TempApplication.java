7a8348becbc5208060a994c1d26a66e5cfcdc3fc
Endri Kurushi <endri-kurushi@hotmail.com>
2019-02-08 12:12:59 +0100

    More something something security

--- src/main/java/thingy/temp/TempApplication.java
+++ src/main/java/thingy/temp/TempApplication.java
@@ -3,8 +3,10 @@ package thingy.temp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class TempApplication {
 	
 	public static void main(String[] args) {
---
+++ src/main/java/thingy/temp/config/AuthorizationServerConfig.java
@@ -0,0 +1,4 @@
+package thingy.temp.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
+import org.springframework.context.annotation.Configuration;
